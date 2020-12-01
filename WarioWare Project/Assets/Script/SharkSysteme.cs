7446350dc1b0e90be87e75c863009937d21f7df6
Guillaume <g.roge@rubika-edu.com>
2020-12-01 10:54:50 +0100

    Spawn requin done

--- WarioWare Project/Assets/Script/SharkSysteme.cs
+++ WarioWare Project/Assets/Script/SharkSysteme.cs
@@ -4,32 +4,47 @@ using UnityEngine;
 
 public class SharkSysteme : MonoBehaviour
 {
+    #region Variable
     public static SharkSysteme Instance;
 
-
-    public bool lockCanSpawn;
-    public bool sharkIsHere;
-
+    [Header ("Temps que le shark reste")]
     public float effectiveTime;
+
+    [Header ("Temps que le requin spawn après l'alerte")]
+    public float timeForSpawn;
+
+    [Header ("Attente min entre chaque alerte")]
     public float timeBtwSpawn;
+
+    [Header ("Temps avant lancement premier spawn")]
     public float minStartSpawn;
 
+    [Header ("Bool qui permet ou non l'apparition du shark")]
+    public bool lockCanSpawn;
+
+    [Header ("Bool qui detecte la presence du shark")]
+    public bool sharkIsHere;
+
+    [Space]
     public GameObject shark;
     public GameObject stopSign;
-
-    private GameObject actualShark;
-
     public Transform sharkPoint;
 
+    private GameObject actualShark;
+    private Animator signAnim;
+    #endregion
 
     void Start()
     {
         ManagerInit();
+        signAnim = stopSign.GetComponent<Animator>();
 
-        lockCanSpawn = false;
+        lockCanSpawn = true;
         sharkIsHere = false;
 
         stopSign.SetActive(false);
+
+        StartCoroutine(StartTime());
     }
 
     void Update()
@@ -58,18 +73,24 @@ public class SharkSysteme : MonoBehaviour
         }
     }
 
+
+    // Coroutine qui fait spawn le shark de manière aléatoire
     IEnumerator SharkSpawnRandom()
     {
-        float cooldown = Random.Range(1, 4);
+        float cooldown = Random.Range(1, 3);
         yield return new WaitForSeconds(cooldown);
-        actualShark = Instantiate(shark, sharkPoint.transform.position, sharkPoint.transform.rotation);
-        sharkIsHere = true;
 
         stopSign.SetActive(true);
+        yield return new WaitForSeconds(timeForSpawn);
+        signAnim.SetBool("switch", true);
+        actualShark = Instantiate(shark, sharkPoint.transform.position, sharkPoint.transform.rotation);
+
+        sharkIsHere = true;
 
         StartCoroutine(LifeTime());
     }
 
+    // Coroutine qui detruit le shark après un certain temps
     IEnumerator LifeTime()
     {
         yield return new WaitForSeconds(effectiveTime);
@@ -78,6 +99,21 @@ public class SharkSysteme : MonoBehaviour
         stopSign.SetActive(false);
 
         sharkIsHere = false;
+
+        StartCoroutine(Cooldown());
+    }
+
+    // Cooldown entre chaque spawn
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(timeBtwSpawn);
+        lockCanSpawn = false;
+    }
+
+    // Cooldown du start
+    IEnumerator StartTime()
+    {
+        yield return new WaitForSeconds(minStartSpawn);
         lockCanSpawn = false;
     }
 }
